46d6783941cbca163cbfb4f9dbb5a37be6a7c9ce
gagocarrilloedgar <edgargc.upc@gmail.com>
2024-10-12 19:16:32 +0200

    feat(conf): intial auth with theme config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import MillionLint from "@million/lint";
+import createNextIntlPlugin from "next-intl/plugin";
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-export default nextConfig;
+export default MillionLint.next({
+  rsc: true,
+})(withNextIntl(nextConfig));
---
+++ src/auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "select_account",
+        },
+      },
+    }),
+  ],
+});
+
+export default handlers;
