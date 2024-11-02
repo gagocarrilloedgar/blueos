d448d9ff020a8e1b52ec8974cec1fe631871b8db
gagocarrilloedgar <edgargc.upc@gmail.com>
2024-11-02 13:35:26 +0100

    feat(frontend): fully working edit project sidepanel

--- frontend/src/pages/dashboard/EditProject/EditProject.tsx
+++ frontend/src/pages/dashboard/EditProject/EditProject.tsx
@@ -1,43 +1,45 @@
 import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
+  Form,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Loader } from "@/components/ui/loader";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
 } from "@/components/ui/select";
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetFooter,
-    SheetHeader,
-    SheetTitle
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle
 } from "@/components/ui/sheet";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "sonner";
 import { z } from "zod";
 import { useLayoutContext } from "../useLayoutContext";
 
 const ProjecSchema = z.object({
   name: z.string(),
   description: z.string(),
-  workedHours: z.number(),
-  clientId: z.number()
+  workedHours: z.coerce.number(),
+  clientId: z.coerce.number()
 });
 
 type Client = {
@@ -61,9 +63,35 @@ export const EditProject = () => {
 const EditProjectForm = () => {
   const { projectId } = useParams();
   const { activeOrg } = useLayoutContext();
+  const [showCreateClient, setShowCreateClient] = useState(false);
+
+  const { isPending, mutate: updateProject } = useMutation({
+    mutationFn: async (data: z.infer<typeof ProjecSchema>) => {
+      const fetchPromise = fetch(
+        `http://localhost:3000/api/v1/projects/${projectId}`,
+        {
+          credentials: "include",
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            id: Number(projectId),
+            ...data
+          })
+        }
+      );
+
+      return toast.promise(fetchPromise, {
+        loading: "Updating project...",
+        success: "Project updated successfully",
+        error: "Failed to update project"
+      });
+    }
+  });
 
   const onSubmit = (data: z.infer<typeof ProjecSchema>) => {
-    console.log(data);
+    updateProject(data);
   };
 
   const { data, isLoading } = useQuery<Project>({
@@ -75,7 +103,7 @@ const EditProjectForm = () => {
     enabled: !!projectId
   });
 
-  const { data: clients } = useQuery({
+  const { data: clients } = useQuery<Client[]>({
     queryKey: ["clients", activeOrg?.id],
     queryFn: () =>
       fetch(
@@ -101,11 +129,11 @@ const EditProjectForm = () => {
         clientId: data.client?.id
       });
     }
-  }, [data?.name]);
+  }, [data]);
 
   const showNoClients = clients?.length === 0;
 
-  console.log(clients);
+  const isDirty = form.formState.isDirty;
 
   if (isLoading)
     return (
@@ -115,81 +143,195 @@ const EditProjectForm = () => {
     );
 
   return (
+    <div className="flex flex-col h-full">
+      <div className="flex-1">
+        <Form {...form}>
+          <form
+            id="edit-project-form"
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col space-y-4 py-4"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="workedHours"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Worked hours</FormLabel>
+                  <Input type="number" {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="clientId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Client</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    value={field.value?.toString()}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a client" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {clients?.map((client: Client) => (
+                        <SelectItem
+                          key={client.id}
+                          value={client.id.toString()}
+                        >
+                          {client.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                    <FormDescription>
+                      {showNoClients
+                        ? "You have no clients yet"
+                        : "Create a new client below if needed"}
+                    </FormDescription>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <CreateNewClient
+          noClients={showNoClients}
+          showCreateClient={showCreateClient}
+          setShowCreateClient={setShowCreateClient}
+        />
+      </div>
+      <SheetFooter className="sticky bottom-4">
+        <Button
+          disabled={isPending || !isDirty}
+          form={showCreateClient ? "create-client-form" : "edit-project-form"}
+          type="submit"
+        >
+          {showCreateClient ? "Create client" : "Save"}
+        </Button>
+      </SheetFooter>
+    </div>
+  );
+};
+
+const CreateNewClient = ({
+  noClients,
+  showCreateClient,
+  setShowCreateClient
+}: {
+  noClients: boolean;
+  showCreateClient: boolean;
+  setShowCreateClient: (show: boolean) => void;
+}) => {
+  const { activeOrg } = useLayoutContext();
+  const queryClient = useQueryClient();
+
+  const form = useForm<{ name: string }>({
+    resolver: zodResolver(
+      z.object({
+        name: z
+          .string({
+            required_error: "Name is required"
+          })
+          .min(1, { message: "Name is required" })
+      })
+    ),
+    mode: "onSubmit"
+  });
+
+  const clientMutation = useMutation({
+    mutationFn: async (data: { name: string; organisationId: number }) => {
+      const fetchPromise = fetch(`http://localhost:3000/api/v1/clients`, {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+
+      return toast.promise(fetchPromise, {
+        loading: "Creating client...",
+        success: "Client created successfully",
+        error: "Failed to create client"
+      });
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ["clients", activeOrg?.id]
+      });
+      form.reset();
+      setShowCreateClient(false);
+    }
+  });
+
+  const onSubmit = (data: { name: string }) => {
+    if (!activeOrg?.id) return;
+
+    clientMutation.mutate({
+      name: data.name,
+      organisationId: activeOrg.id
+    });
+  };
+
+  if (!showCreateClient)
+    return (
+      <div className="flex flex-col gap-2 items-center">
+        <Button
+          onClick={() => setShowCreateClient(true)}
+          type="button"
+          size="sm"
+          className="w-full"
+          variant="ghost"
+        >
+          <Plus className="mr-2 w-4 h-4" />
+          {noClients ? "Create client" : "New client"}
+        </Button>
+      </div>
+    );
+
+  return (
     <Form {...form}>
-      <form
-        id="edit-project-form"
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col space-y-4 py-4"
-      >
+      <form onSubmit={form.handleSubmit(onSubmit)} id="create-client-form">
         <FormField
           control={form.control}
           name="name"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Name</FormLabel>
+              <FormLabel>Client name</FormLabel>
               <Input {...field} />
               <FormMessage />
+              <FormDescription>
+                This client will be created and linked to your organisation. For
+                visibility you'll have to assign it to a client portal
+              </FormDescription>
             </FormItem>
           )}
         />
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <Input {...field} />
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="workedHours"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Worked hours</FormLabel>
-              <Input type="number" {...field} />
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="clientId"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Client</FormLabel>
-              <Select
-                onValueChange={field.onChange}
-                value={field.value?.toString()}
-              >
-                <SelectTrigger>
-                  <SelectValue placeholder="Select a client" />
-                </SelectTrigger>
-                <SelectContent>
-                  {clients?.map((client: Client) => (
-                    <SelectItem key={client.id} value={client.id.toString()}>
-                      {client.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-                <FormDescription>
-                  {showNoClients && "You have no clients yet"}
-                </FormDescription>
-              </Select>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {showNoClients && (
-          <div className="flex flex-col gap-2 items-center">
-            <Button type="button" size="sm" className="w-full" variant="ghost">
-              Create client
-            </Button>
-          </div>
-        )}
       </form>
     </Form>
   );
@@ -209,11 +351,6 @@ const EditProjectLayout = ({ children }: { children: React.ReactNode }) => {
           </SheetDescription>
         </SheetHeader>
         {children}
-        <SheetFooter>
-          <Button form="edit-project-form" type="submit">
-            Save
-          </Button>
-        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
